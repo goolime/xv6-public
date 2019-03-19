c205fa4d8cce54a05ee45d0cc23d5089e85c69b1
Unknown <goolime@gmail.com>
2019-03-19 19:06:13 +0200

    proc.c

--- proc.h
+++ proc.h
@@ -51,6 +51,7 @@ struct proc {
   struct file *ofile[NOFILE];    // Open files
   struct inode *cwd;             // Current directory  
   char name[16];                 // Process name (debugging)
+  int status;                    // Save exit status (task 2)
 };
 
 // Process memory is laid out contiguously, low addresses first:
